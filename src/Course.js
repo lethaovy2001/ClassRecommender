5ba682adb00ce14aed4924b1e2faa876019ca1d7
Bilge Mutlu <bmutlu@wisc.edu>
2019-10-05 11:31:07 -0500

    Initial commit

---
+++ src/Course.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Card from 'react-bootstrap/Card';
+
+class Course extends React.Component {
+  render() {
+    return (
+      <Card style={{width: '33%', marginTop: '5px', marginBottom: '5px'}}>
+        <Card.Body>
+          <Card.Title>{this.props.data.name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{this.props.data.number} - {this.getCredits()}</Card.Subtitle>
+        </Card.Body>
+      </Card>
+    )
+  }
+
+  getCredits() {
+    if(this.props.data.credits === 1)
+      return '1 credit';
+    else
+      return this.props.data.credits + ' credits';
+  }
+}
+
+export default Course;
